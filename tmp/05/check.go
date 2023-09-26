603f4fd2ed9c7205eb428a4bb2669224ffc5bee0
tf63 <tfukuda9031@gmail.com>
2023-09-26 09:28:08 +0000

    add tutorial file

---
+++ tmp/05/check.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"os"
+
+	"github.com/tf63/code-api/tmp/utils"
+)
+
+func main() {
+	title := ""
+	done := ""
+	ticketIdx := 5
+
+	if err := utils.Check(title, done, ticketIdx); err != nil {
+		println("invalid value")
+		os.Exit(1)
+	}
+
+	println("success")
+}
