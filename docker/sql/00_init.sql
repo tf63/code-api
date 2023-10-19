e0de54b80eb8aaf383d14ee715598ca2152e9cb6
tf63 <tfukuda9031@gmail.com>
2023-10-19 05:14:40 +0000

    feat: add ddl & dml

--- docker/sql/00_init.sql
+++ docker/sql/00_init.sql
@@ -1,5 +1,73 @@
-CREATE TABLE IF NOT EXISTS todos (
+CREATE TABLE IF NOT EXISTS todo (
     id SERIAL PRIMARY KEY,
     title varchar(50) NOT NULL,
     done BOOLEAN DEFAULT false
 );
+COMMENT ON TABLE todo IS 'todoを管理するテーブル (サンプル)';
+
+CREATE TABLE IF NOT EXISTS algorithm (
+    algorithm_id BIGINT PRIMARY KEY,
+    name varchar(50) NOT NULL
+);
+COMMENT ON TABLE algorithm IS 'algorithmを管理するテーブル';
+
+CREATE TABLE IF NOT EXISTS pattern (
+    pattern_id BIGINT PRIMARY KEY,
+    name varchar(50) NOT NULL
+);
+COMMENT ON TABLE pattern IS 'designpatternを管理するテーブル';
+
+CREATE TABLE IF NOT EXISTS language (
+    language_id BIGINT PRIMARY KEY,
+    name varchar(50) NOT NULL
+);
+COMMENT ON TABLE language IS 'languageを管理するテーブル';
+
+CREATE TABLE IF NOT EXISTS framework (
+    framework_id BIGINT PRIMARY KEY,
+    name varchar(50) NOT NULL
+);
+COMMENT ON TABLE framework IS 'frameworkを管理するテーブル';
+
+CREATE TABLE IF NOT EXISTS tool (
+    tool_id BIGINT PRIMARY KEY,
+    language_id BIGINT,
+    framework_id BIGINT DEFAULT 1,
+    FOREIGN KEY (language_id) REFERENCES public.language(language_id) ON DELETE RESTRICT,
+    FOREIGN KEY (framework_id) REFERENCES public.framework(framework_id) ON DELETE SET DEFAULT
+);
+COMMENT ON TABLE tool IS 'frameworkとlanguageをまとめるテーブル';
+
+CREATE TABLE IF NOT EXISTS program_code (
+    program_code_id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    nrow BIGINT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    tool_id BIGINT,
+    FOREIGN KEY (tool_id) REFERENCES public.tool(tool_id) ON DELETE RESTRICT
+);
+COMMENT ON TABLE program_code IS 'プログラミング言語のソースコードを管理するテーブル';
+
+CREATE TABLE IF NOT EXISTS algorithm_code (
+    algorithm_code_id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    nrow BIGINT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    language_id BIGINT,
+    algorithm_id BIGINT,
+    FOREIGN KEY (language_id) REFERENCES public.language(language_id) ON DELETE RESTRICT,
+    FOREIGN KEY (algorithm_id) REFERENCES public.algorithm(algorithm_id) ON DELETE RESTRICT
+);
+COMMENT ON TABLE algorithm_code IS '様々なプログラミング言語で書かれたアルゴリズムのソースコードを管理するテーブル';
+
+CREATE TABLE IF NOT EXISTS pattern_code (
+    pattern_code_id SERIAL PRIMARY KEY,
+    content TEXT NOT NULL,
+    nrow BIGINT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT now(),
+    language_id BIGINT,
+    pattern_id BIGINT,
+    FOREIGN KEY (language_id) REFERENCES public.language(language_id) ON DELETE RESTRICT,
+    FOREIGN KEY (pattern_id) REFERENCES public.pattern(pattern_id) ON DELETE RESTRICT
+);
+COMMENT ON TABLE pattern_code IS '様々なプログラミング言語で書かれたデザインパターンのソースコードを管理するテーブル';
--- docker/sql/01_seed.sql
+++ docker/sql/01_seed.sql
@@ -1,3 +1,46 @@
-INSERT INTO todos (title, done) 
+INSERT INTO todo (title, done) 
     VALUES ('todo 1', false), ('todo 2', true), ('todo 3', true), ('todo 4', true), ('todo 5', false), ('todo 6', true), ('todo 7', true), ('todo 8', false), ('todo 9', true), ('todo 10', false)
+;
+
+-- Master
+INSERT INTO language (language_id, name)
+    VALUES (1, 'python'), (2, 'go'), (3, 'typescript'), (4, 'javascript')
+;
+
+INSERT INTO framework (framework_id, name)
+    VALUES (1, 'something'), 
+            (2, 'fastapi'), (3, 'flask'), (4, 'django'), 
+            (5, 'react'), (6, 'nextjs'), (7, 'vue'), (8, 'nuxtjs'), (9, 'nodejs'), (10, 'express')
+;
+
+INSERT INTO tool (language_id, framework_id, tool_id)
+    VALUES (1, 1, 1), (1, 2, 2), (1, 3, 3),
+            (2, 1, 4), 
+            (3, 1, 5), (3, 5, 6), (3, 6, 7), (3, 7, 8), (3, 8, 9), (3, 9, 10), (3, 10, 11),
+            (4, 1, 12), (4, 5, 13), (4, 6, 14), (4, 7, 15), (4, 8, 16), (4, 9, 17), (4, 10, 18)
+;
+
+INSERT INTO algorithm (algorithm_id, name)
+    VALUES (1, 'dfs'), (2, 'bfs'), (3, 'dp'), (4, 'binary search')
+;
+
+INSERT INTO pattern (pattern_id, name)
+    VALUES (1, 'abstract factory pattern'), (2, 'builder pattern'), (3, 'factory method pattern'), (4, 'prototype pattern'), (5, 'singleton pattern')
+;
+
+-- not Master ?
+INSERT INTO program_code (content, nrow, tool_id)
+    VALUES ('this is python code', 1, 1),
+            ('this is fastapi code', 1, 2),
+            ('this is go code', 1, 4)
+;
+
+INSERT INTO algorithm_code (content, nrow, algorithm_id, language_id)
+    VALUES ('this is python dfs code', 1, 1, 1),
+            ('this is go dfs code', 1, 1, 2)
+;
+
+INSERT INTO pattern_code (content, nrow, pattern_id, language_id)
+    VALUES ('this is python abstract factory pattern code', 1, 1, 1),
+        ('this is typescript abstract factory pattern code', 1, 1, 3)
 ;
