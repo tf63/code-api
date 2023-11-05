4cd76f85276c43c1ca6cc871a80962948a1e0713
tf63 <tfukuda9031@gmail.com>
2023-11-05 15:27:41 +0900

    add redis env

--- external/redis.go
+++ external/redis.go
@@ -1,15 +1,21 @@
 package external
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/redis/go-redis/v9"
 )
 
 // Redisに接続し，Clientオブジェクトを返す
 func ConnectRedis() (*redis.Client, error) {
+	addr := fmt.Sprintf("%s:%s",
+		os.Getenv("REDIS_HOST"),
+		os.Getenv("REDIS_PORT"))
 
 	rdb := redis.NewClient(&redis.Options{
-		Addr:     "redis:6379",
-		Password: "",
+		Addr:     addr,
+		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
 
