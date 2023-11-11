743821adb82714fa2111758b3725c937f418e040
tf63 <tfukuda9031@gmail.com>
2023-11-12 01:38:59 +0900

    add db seed

--- bin/seed.sh
+++ bin/seed.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-mkdir -p bin/data
-source bin/seed/init.sh
+mkdir -p bin/seed
+source bin/init.sh
 # ----------------------------------------------------------------
 # language
 # ----------------------------------------------------------------
-echo "language_id,name" > bin/data/language.csv
+echo "language_id,name" > bin/seed/language.csv
 for language in "${!LANGUAGE_ID_MAP[@]}"; do
-    echo "${LANGUAGE_ID_MAP[$language]},${language}" >> bin/data/language.csv
+    echo "${LANGUAGE_ID_MAP[$language]},${language}" >> bin/seed/language.csv
 done
 # ----------------------------------------------------------------
 # framework
 # ----------------------------------------------------------------
-echo "framework_id,name" > bin/data/framework.csv
+echo "framework_id,name" > bin/seed/framework.csv
 for framework in "${!FRAMEWORK_ID_MAP[@]}"; do
-    echo "${FRAMEWORK_ID_MAP[$framework]},${framework}" >> bin/data/framework.csv
+    echo "${FRAMEWORK_ID_MAP[$framework]},${framework}" >> bin/seed/framework.csv
 done
 # ----------------------------------------------------------------
 # tool
 # ----------------------------------------------------------------
-echo "tool_id,language_id,framework_id" > bin/data/tool.csv
+echo "tool_id,language_id,framework_id" > bin/seed/tool.csv
 for tool in "${!TOOL_ID_MAP[@]}"; do
     language=`echo ${tool} | cut -d'-' -f1`
     framework=`echo ${tool} | cut -d'-' -f2`
-    echo "${TOOL_ID_MAP[$tool]},${LANGUAGE_ID_MAP[${language}]},${FRAMEWORK_ID_MAP[${framework}]}" >> bin/data/tool.csv
+    echo "${TOOL_ID_MAP[$tool]},${LANGUAGE_ID_MAP[${language}]},${FRAMEWORK_ID_MAP[${framework}]}" >> bin/seed/tool.csv
 done
 # ----------------------------------------------------------------
 # algorithm
 # ----------------------------------------------------------------
-echo "algorithm_id,name" > bin/data/algorithm.csv
+echo "algorithm_id,name" > bin/seed/algorithm.csv
 for algorithm in "${!ALGORITHM_ID_MAP[@]}"; do
-    echo "${ALGORITHM_ID_MAP[$algorithm]},${algorithm}" >> bin/data/algorithm.csv
+    echo "${ALGORITHM_ID_MAP[$algorithm]},${algorithm}" >> bin/seed/algorithm.csv
 done
 # ----------------------------------------------------------------
 # pattern
 # ----------------------------------------------------------------
-echo "pattern_id,name" > bin/data/pattern.csv
+echo "pattern_id,name" > bin/seed/pattern.csv
 for pattern in "${!PATTERN_ID_MAP[@]}"; do
-    echo "${PATTERN_ID_MAP[$pattern]},${pattern}" >> bin/data/pattern.csv
+    echo "${PATTERN_ID_MAP[$pattern]},${pattern}" >> bin/seed/pattern.csv
 done
--- docker/sql/01_seed.sql
+++ docker/sql/01_seed.sql
@@ -3,6 +3,7 @@
 \COPY framework FROM /data/framework.csv DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
 \COPY algorithm FROM /data/algorithm.csv DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
 \COPY tool FROM /data/tool.csv DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
-\COPY program_code ("nrow", "tool_id", "content") FROM /data/program_code.csv DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
+\COPY language_code ("nrow", "language_id", "content") FROM /data/language_code.csv DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
+\COPY framework_code ("nrow", "tool_id", "content") FROM /data/framework_code.csv DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
 \COPY algorithm_code ("nrow", "language_id", "algorithm_id", "content") FROM /data/algorithm_code.csv DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
 \COPY pattern_code ("nrow", "language_id", "pattern_id", "content") FROM /data/pattern_code.csv DELIMITER ',' CSV HEADER QUOTE '"' ESCAPE '\';
